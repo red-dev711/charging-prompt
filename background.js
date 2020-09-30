ddc0574d9f9c014b2cc4b5c1f58d77f2a94ebffe
Yash Dubey <44433103+Y-dub@users.noreply.github.com>
2020-10-01 03:03:12 +0530

    Removed comments

--- background.js
+++ background.js
@@ -1,22 +1,4 @@
-// let tabId;
-
-// chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
-//   tabId = tab[0].id;
-//   title = tab[0].title;
-//   url = tab[0].url;
-//   chrome.idle.setDetectionInterval(15);
-
-//   chrome.idle.onStateChanged.addListener(function (newState) {
-//     if (newState == "idle") {
-//       console.log(newState);
-//       console.log(tabId, title, url);
-//       // chrome.tabs.sendMessage(tabId, { state: newState }, function (response) {
-//       //   console.log(chrome.runtime.lastError);
-//       // });
-//     }
-//   });
-// });
-// tabID will have the id value only after this callback function is executed (and callback functions are not executed immediately) so if we want to use this tabId value somewhere then we have to include that inside this callback function.
+// Removed all the unnecessary comments
 
 chrome.idle.setDetectionInterval(15*60);
 
